7da50bbed4a1317520d2aa99745ed7ded4bf123d
Tem L <teml@Tems-MacBook-Pro.local>
2024-10-07 09:29:33 +0100

    cleaned up files

--- vm.tf
+++ vm.tf
@@ -14,12 +14,6 @@ resource "azurerm_virtual_machine" "vm1" {
   network_interface_ids         = [azurerm_network_interface.nic1.id]
   vm_size                       = "Standard_B1s"
   delete_os_disk_on_termination = true
-  #admin_username        = "azureuser"
-  #availability_set_id   = azurerm_availability_set.avset.id
-  #   admin_ssh_key {
-  #     username   = "azureuser"
-  #     public_key = file("${var.public_key_loc}")
-  #   }
 
   storage_os_disk {
     name              = "${random_pet.prefix.id}-osdisk"
@@ -57,12 +51,6 @@ resource "azurerm_virtual_machine" "vm2" {
   network_interface_ids         = [azurerm_network_interface.nic2.id]
   vm_size                       = "Standard_B1s"
   delete_os_disk_on_termination = true
-  #admin_username        = "azureuser"
-  #availability_set_id   = azurerm_availability_set.avset.id
-  #   admin_ssh_key {
-  #     username   = "azureuser"
-  #     public_key = file("${var.public_key_loc}")
-  #   }
 
   storage_os_disk {
     name              = "${random_pet.prefix.id}-osdisk"
@@ -92,75 +80,3 @@ resource "azurerm_virtual_machine" "vm2" {
     }
   }
 }
-
-# resource "azurerm_virtual_machine" "vm1" {
-#   name                  = "${random_pet.prefix.id}-vm1"
-#   location              = azurerm_resource_group.rg.location
-#   resource_group_name   = azurerm_resource_group.rg.name
-#   network_interface_ids = [azurerm_network_interface.nic1.id]
-#   vm_size               = "Standard_B1s"
-#   availability_set_id   = azurerm_availability_set.avset.id
-
-#   os_profile {
-#     computer_name  = "${random_pet.prefix.id}-vm1"
-#     admin_username = "azureuser"
-#     admin_ssh_key {
-#       username   = "azureuser"
-#       public_key = file("${var.public_key_loc}")
-#     }
-#   }
-
-#   os_profile_linux_config {
-#     disable_password_authentication = true
-#   }
-
-#   storage_os_disk {
-#     name              = "${random_pet.prefix.id}-osdisk"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#     managed_disk_type = "Standard_LRS"
-#   }
-
-#   storage_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-LTS"
-#     version   = "latest"
-#   }
-# }
-
-# resource "azurerm_virtual_machine" "vm1" {
-#   name                  = "${random_pet.prefix.id}-vm1"
-#   location              = azurerm_resource_group.rg.location
-#   resource_group_name   = azurerm_resource_group.rg.name
-#   network_interface_ids = [azurerm_network_interface.nic1.id]
-#   vm_size               = "Standard_B1s"
-#   availability_set_id   = azurerm_availability_set.avset.id
-
-#   os_profile {
-#     computer_name  = "${random_pet.prefix.id}-vm1"
-#     admin_username = "azureuser"
-#     admin_ssh_key {
-#       username   = "azureuser"
-#       public_key = file("${var.public_key_loc}")
-#     }
-#   }
-
-#   os_profile_linux_config {
-#     disable_password_authentication = true
-#   }
-
-#   storage_os_disk {
-#     name              = "${random_pet.prefix.id}-osdisk"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#     managed_disk_type = "Standard_LRS"
-#   }
-
-#   storage_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-LTS"
-#     version   = "latest"
-#   }
-# }
