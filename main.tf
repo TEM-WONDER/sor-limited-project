c24153359c23ffd26cedadbc2877b2fd651057e3
Tem L <teml@Tems-MacBook-Pro.local>
2024-10-07 03:22:03 +0100

    Added vnet and subnets

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = "uksouth"
+}
+
+resource "random_pet" "prefix" {
+  length    = 1
+  prefix = "sor"
+}
+
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${random_pet.prefix.id}-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a subnet within the virtual network
+resource "azurerm_subnet" "my_terraform_subnet_1" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_subnet" "my_terraform_subnet_2" {
+  name                 = "subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
